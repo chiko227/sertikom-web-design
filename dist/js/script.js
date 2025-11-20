eef6e4dbbc3d3ce0a11a7aba4734811d76dfc4c4
chiko227 <rafifchiko2@gamil.com>
2025-11-20 15:25:10 +0700

    penambahan web sertikom

---
+++ dist/js/script.js
@@ -0,0 +1,3 @@
+<div class="col-lg-6 text-center">
+                    <img src="asset/img/264e268c91acc0d2ea4799b44733597d.jpg_wh860-removebg-preview.png" alt="Ilustrasi Website" class="hero-img ms-5 ps-5">
+                </div>
